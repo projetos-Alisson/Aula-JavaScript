23ccadfd5de9a20ad3c0aae534caffdb402231f4
Alisson Soares <79875609+projetos-Alisson@users.noreply.github.com>
2021-10-26 09:51:10 -0300

    Add files via upload
    Calculadora

---
+++ Aula01/entrada de dados/calculadora.js
@@ -0,0 +1,23 @@
+var num1=parseInt(prompt('Digite o num1'));
+var num2=parseInt(prompt('Digite o num2'));
+var soma= num1+num2;
+
+
+var sub= num1-num2;
+
+var mult= num1*num2;
+
+var div= num1/num2;
+
+document.getElementById('num1').innerHTML = num1;
+document.getElementById('num2').innerHTML = num2;
+document.getElementById('soma').innerHTML = soma;
+document.getElementById('sub').innerHTML = sub;
+document.getElementById('mult').innerHTML = mult;
+document.getElementById('div').innerHTML = div;
+
+//console.log(soma);
+
+
+
+
